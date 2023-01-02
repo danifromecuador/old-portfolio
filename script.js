7f1d98eb62ad6ec7604821adf9299ec465a519b8
dani morillo <danifromecuador@gmail.com>
2023-01-02 17:56:24 -0500

    Fix linter errors on JS file

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let menu = document.querySelector('.menu');
-let hamburger = document.querySelector('.hamburger-icon-menu');
+const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.hamburger-icon-menu');
 
 function display() {
   hamburger.classList.toggle('active');
 }
 
-menu.addEventListener('click',display);
+menu.addEventListener('click', display);
