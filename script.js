56d3d0a2c571063d95ba1bbe0688417a5cfd3a6d
dani morillo <danifromecuador@gmail.com>
2023-01-04 10:24:10 -0500

    I can't center the div of hamburger icon menu

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const menu = document.querySelector('.menu');
 const hamburger = document.querySelector('.hamburger-icon-menu');
+const canvas = document.querySelector('.canvas');
 
 function display() {
   hamburger.classList.toggle('active');
+  // canvas.classList.toggle('inactive');
 }
 
 menu.addEventListener('click', display);
