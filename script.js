5119a389dd829963bccc7cb359edef61ce5612ba
dani morillo <danifromecuador@gmail.com>
2023-01-04 15:34:05 -0500

    Fix linter errors

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const hamburgerIcon = document.querySelector('.hamburger-icon');
 const closeIcon = document.querySelector('.close-icon');
 
 function display() {
-  hamburgerMenu.style.display = 'block'
+  hamburgerMenu.style.display = 'block';
 }
 
 hamburgerIcon.addEventListener('click', display);
 
 function noneDisplay() {
-  hamburgerMenu.style.display = 'none'
+  hamburgerMenu.style.display = 'none';
 }
 
 closeIcon.addEventListener('click', noneDisplay);
