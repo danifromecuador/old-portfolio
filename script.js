9fc9b11d932f9fc5ce77c2267ee2cb343eb4eb7a
dani morillo <danifromecuador@gmail.com>
2023-01-04 16:22:19 -0500

    Fix eslint errors

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const hamburgerIcon = document.querySelector('.hamburger-icon');
 const closeIcon = document.querySelector('.close-icon');
 const pacMobile = document.querySelector('.PAC-mobile');
 
-hamburgerIcon.addEventListener('click', () => hamburgerMenu.style.display = 'block');
+hamburgerIcon.addEventListener('click', () => {
+  hamburgerMenu.style.display = 'block';
+  }
+);
+
 closeIcon.addEventListener('click', () => hamburgerMenu.style.display = 'none');
 pacMobile.addEventListener('click', () => hamburgerMenu.style.display = 'none');
