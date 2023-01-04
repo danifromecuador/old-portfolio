d4ad0c4859f724470b847e2d1eda2a5b818f3f8c
dani morillo <danifromecuador@gmail.com>
2023-01-04 16:16:11 -0500

    Create close mobile menu functionality when click on portfolio or about or contact links

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const closeIcon = document.querySelector('.close-icon');
+const pacMobile = document.querySelector('.PAC-mobile');
 
-function display() {
-  hamburgerMenu.style.display = 'block';
-}
-
-hamburgerIcon.addEventListener('click', display);
-
-function noneDisplay() {
-  hamburgerMenu.style.display = 'none';
-}
-
-closeIcon.addEventListener('click', noneDisplay);
+hamburgerIcon.addEventListener('click', () => hamburgerMenu.style.display = 'block');
+closeIcon.addEventListener('click', () => hamburgerMenu.style.display = 'none');
+pacMobile.addEventListener('click', () => hamburgerMenu.style.display = 'none');
